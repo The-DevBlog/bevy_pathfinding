8615b642985603924bfe0eb63eb011745b05f2f1
andrew <andrewcs149@gmail.com>
2025-01-31 09:34:23 -0500

    update

--- src/components.rs
+++ src/components.rs
@@ -28,11 +28,11 @@ pub struct Boid {
 impl Default for Boid {
     fn default() -> Self {
         Self {
-            separation_weight: 50.0,
+            separation_weight: 40.0,
             alignment_weight: 1.0,
             cohesion_weight: 1.0,
             max_speed: 4.0,
-            neighbor_radius: 30.0,
+            neighbor_radius: 25.0,
         }
     }
 }
